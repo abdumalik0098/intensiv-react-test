1565fe0f4291bd03d293bb87436a8e334de4c958
Malik <mister.ak98@list.ru>
2018-07-08 23:05:29 +0700

    Commit_3 for react

--- src/App.js
+++ src/App.js
@@ -3,34 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
 
 class App extends Component {
   render() {
     return (
-    <div className="wrapper">
-      <FullName name="Sami" surname="Yusuf" link="vk.com" />
-      <FullName name="Maher" surname="Zain" link="ok.ru" />
-      <FullName name="Ivan" surname="Ivanov" link="#" />
-    </div>
+          <div className="App">
+
+            <HeaderWrapper>
+                <Grid>
+                    <Header/>
+                </Grid>
+            </HeaderWrapper>
+              
+              <Menu/>
+              <Main/>
+
+          </div>
 
     );
   }
 }
 
+// ///////////////////////////////////////////////////////////////
 
-function FullName (props){
-  return (
-      <div>
-      <h1>Моё имя {props.name}, фамилия - {props.surname}</h1>
-      <a href={props.link} >Link to my page...</a>
-      </div>
-    )
-}
+// class App extends Component {
+//   render() {
+//     return (
+//     <div className="wrapper">
+//       <FullName name="Sami" surname="Yusuf" link="vk.com" />
+//       <FullName name="Maher" surname="Zain" link="ok.ru" />
+//       <FullName name="Ivan" surname="Ivanov" link="#" />
+//     </div>
+
+//     );
+//   }
+// }
+
+
+// function FullName (props){
+//   return (
+//       <div>
+//       <h1>Моё имя {props.name}, фамилия - {props.surname}</h1>
+//       <a href={props.link} >Link to my page...</a>
+//       </div>
+//     )
+// }
 
 
 
